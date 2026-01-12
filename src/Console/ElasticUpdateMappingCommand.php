0f9f26cec4c5378a99c111644e93a96a4328d1e2
Kenneth Kariuki <kenneth@area17.com>
2026-01-13 01:19:45 +0300

    Update payload on putMapping for es 8 and 9 compatibility

--- src/Console/ElasticUpdateMappingCommand.php
+++ src/Console/ElasticUpdateMappingCommand.php
@@ -46,7 +46,7 @@ class ElasticUpdateMappingCommand extends Command
         }
 
         $payload = (new TypePayload($model))
-            ->set('body.'.$model->searchableAs(), $mapping)
+            ->set('body', $mapping)
             ->set('include_type_name', 'true');
 
         if (in_array(Migratable::class, class_uses_recursive($configurator))) {
